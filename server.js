8b987d6a5df0251d986dc9d0122d99336adda076
Tia Low <lowtia@gmail.com>
2020-07-13 15:46:11 -0700

    Scaffolded files, initiated connection using PORT, updated README

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+
+const app = express();
+
+require('dotenv').config();
+
+const cors = require('cors');
+
+app.use(cors());
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+})
+
+
+
+
+
+
